35cd8351b21db7ee84ade7fa70c1d948baf7a882
June <norisori@gmail.com>
2026-03-23 23:56:18 +0100

    Added .github workflows and edited package.json

---
+++ frontend/src/App.test.tsx
@@ -0,0 +1,7 @@
+import { describe, it, expect } from 'vitest';
+
+describe('Basic Math Test for CI', () => {
+    it('should confirm that 1 + 1 is 2', () => {
+        expect(1 + 1).toBe(2);
+    });
+});
