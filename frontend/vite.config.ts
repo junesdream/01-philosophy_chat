d5c6f4294a047ae638fc0bda9a6679c071906143
June <norisori@gmail.com>
2026-03-24 00:03:40 +0100

    Edited main.yml, package.json, vite.config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    // @ts-ignore
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './src/setupTests.ts', // Falls du globale Imports für Tests willst
+    },
 })
